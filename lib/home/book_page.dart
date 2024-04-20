958f5107db3d9e205b071236922af0fc76448cba
KavishkaSasindu <sasindu0215@gmail.com>
2024-04-20 21:16:23 +0530

    updated ksdev

--- lib/home/book_page.dart
+++ lib/home/book_page.dart
@@ -1,5 +1,6 @@
 import "package:flutter/material.dart";
 import "package:tourism_app/reusable/book_page/book_card.dart";
+import "package:tourism_app/shared/button.dart";
 import "package:tourism_app/utils/color.dart";
 import "package:tourism_app/widgets/book_page/place_card.dart";
 
@@ -24,28 +25,28 @@ class BookTour extends StatelessWidget {
               fontWeight: FontWeight.w500),
         ),
       ),
-      body: const SingleChildScrollView(
+      body: SingleChildScrollView(
         child: Padding(
           padding: EdgeInsets.all(10.0),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              Text(
+              const Text(
                   "Welcome to our travel app, your ultimate guide to discovering captivating destinations around the globe! Whether you're seeking the tranquility visit offers something for every traveler."),
-              SizedBox(
+              const SizedBox(
                 height: 10,
               ),
-              Text(
+              const Text(
                 "Select a Vehicle",
                 style: TextStyle(
                     color: mainPurpleColor,
                     fontSize: 20,
                     fontWeight: FontWeight.bold),
               ),
-              SizedBox(
+              const SizedBox(
                 height: 10,
               ),
-              Row(
+              const Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
                   BookCard(
@@ -62,20 +63,87 @@ class BookTour extends StatelessWidget {
                   ),
                 ],
               ),
-              SizedBox(
+              const SizedBox(
                 height: 10,
               ),
-              Text(
+              const Text(
                 "Selected Place",
                 style: TextStyle(
                     color: mainPurpleColor,
                     fontSize: 20,
                     fontWeight: FontWeight.bold),
               ),
-              SizedBox(
+              const SizedBox(
                 height: 10,
               ),
               PlaceCard(),
+              const SizedBox(
+                height: 10,
+              ),
+              const Text(
+                "Fill The Details",
+                style: TextStyle(
+                    fontSize: 20,
+                    color: mainPurpleColor,
+                    fontWeight: FontWeight.bold),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              const Text(
+                "User Name",
+                style: TextStyle(color: Color(0xff6E7A76), fontSize: 16),
+              ),
+              TextField(
+                decoration: InputDecoration(
+                    contentPadding: const EdgeInsets.symmetric(
+                        horizontal: 20, vertical: 15),
+                    hintText: "John...",
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(20)),
+                    hintStyle: const TextStyle(color: Color(0xff6E7A76))),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              const Text(
+                "Country",
+                style: TextStyle(color: Color(0xff6E7A76), fontSize: 16),
+              ),
+              TextField(
+                decoration: InputDecoration(
+                    contentPadding: const EdgeInsets.symmetric(
+                        horizontal: 20, vertical: 15),
+                    hintText: "Canada...",
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(20)),
+                    hintStyle: const TextStyle(color: Color(0xff6E7A76))),
+              ),
+              const SizedBox(
+                height: 15,
+              ),
+              Container(
+                width: double.infinity,
+                height: 2,
+                decoration: BoxDecoration(color: mainTextColor),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              Text(
+                "Welcome to our travel app, your ultimate guide to discovering captivating destinations around the globe! Whether you're seeking the tranquility visit offers something for every traveler.",
+                style: TextStyle(
+                    color: mainTextColor,
+                    fontSize: 15,
+                    fontWeight: FontWeight.w500),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              const Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [ButtonBox(buttonValue: "Submit")],
+              )
             ],
           ),
         ),
